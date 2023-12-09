42c143f6fcc1ee58ee3daac501cc06dadc67fa8c
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-09 16:31:25 +0100

    App includes Counter

--- 01-lesson/src/App.js
+++ 01-lesson/src/App.js
@@ -1,5 +1,11 @@
+import Counter from './features/counter/Counter'
+
 function App() {
-    return <div>App</div>
+    return (
+        <main className="App">
+            <Counter />
+        </main>
+    )
 }
 
 export default App
