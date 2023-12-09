be94e125b392dea705fc0c084c2abb53ccc63da5
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-09 17:37:48 +0100

    AddPost in App

--- 02-lesson/src/App.js
+++ 02-lesson/src/App.js
@@ -1,8 +1,10 @@
+import AddPost from './features/posts/AddPost'
 import PostsList from './features/posts/PostsList'
 
 function App() {
     return (
         <main className="App">
+            <AddPost />
             <PostsList />
         </main>
     )
