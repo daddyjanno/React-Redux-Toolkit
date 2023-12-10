2ae3009ddeea941f3473a594663826f6f3144c67
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-10 17:11:13 +0100

    dispatch userFetch directly in the store

--- 03_lesson/src/index.js
+++ 03_lesson/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { fetchUsers } from './features/users/usersSlice';
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
