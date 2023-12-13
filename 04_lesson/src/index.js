12ef2580fbf7c04dd2297b15d8a5b21e05d4647d
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-13 17:54:43 +0100

    index now dispatch fetchPosts

--- 04_lesson/src/index.js
+++ 04_lesson/src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { fetchUsers } from './features/users/usersSlice';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
+import { fetchPosts } from './features/posts/postsSlice'
 
+store.dispatch(fetchPosts())
 store.dispatch(fetchUsers())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path='/*' element={<App />}/>
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
