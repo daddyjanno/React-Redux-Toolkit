ecbb08b939123dc7c7ade9798037a40129defd49
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-13 16:37:37 +0100

    new selector to find a specific post

--- 04_lesson/src/features/posts/postsSlice.js
+++ 04_lesson/src/features/posts/postsSlice.js
@@ -111,7 +111,10 @@ const postsSlice = createSlice({
 
 
 // selectors
-export const selectAllPosts = (state) => state.posts.posts;
+export const selectAllPosts = (state) => state.posts.posts
+export const selectPostById = (state, postId) =>
+    state.posts.posts.find((post) => post.id === postId)
+
 export const getPostStatus = (state) => state.posts.status
 export const getPostsError = (state) => state.posts.error
 
