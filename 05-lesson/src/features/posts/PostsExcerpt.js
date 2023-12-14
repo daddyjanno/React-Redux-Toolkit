b2ea64ceb439c60b26f7f087a849eb4ea040c47f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-14 14:09:38 +0100

    PostExercp now with postId

--- 05-lesson/src/features/posts/PostsExcerpt.js
+++ 05-lesson/src/features/posts/PostsExcerpt.js
@@ -2,8 +2,13 @@ import PostAuthor from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
 import { Link } from 'react-router-dom'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectPostById } from './postsSlice'
 
-const PostsExcerpt = ({ post }) => {
+const PostsExcerpt = ({ postId }) => {
+    const post = useSelector((state) => selectPostById(state, postId))
+
     return (
         <article>
             <h2>{post.title}</h2>
