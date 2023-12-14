370ab5ae84c120df739512cfc9d0931f315536a6
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-14 13:31:09 +0100

    New Routes for UsersList and UserPage

--- 05-lesson/src/App.js
+++ 05-lesson/src/App.js
@@ -1,9 +1,11 @@
 import PostsList from './features/posts/PostsList'
 import AddPostForm from './features/posts/AddPostForm'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import SinglePostPage from './features/posts/SinglePostPage'
 import EditPostForm from './features/posts/EditPostForm'
+import UsersList from './features/users/UsersList'
+import UserPage from './features/users/UserPage'
 
 function App() {
     return (
@@ -16,6 +18,14 @@ function App() {
                     <Route path=":postId" element={<SinglePostPage />} />
                     <Route path="edit/:postId" element={<EditPostForm />} />
                 </Route>
+
+                <Route path="user">
+                    <Route index element={<UsersList />} />
+                    <Route path=":userId" element={<UserPage />} />
+                </Route>
+
+                {/* Catch all - replace with 404 component if you want*/}
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
         </Routes>
     )
