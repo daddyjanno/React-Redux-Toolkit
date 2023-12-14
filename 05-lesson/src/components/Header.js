1900ba7c9c22077524dd41d11e91715e95c9716e
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-14 13:31:19 +0100

    count and count button

--- 05-lesson/src/components/Header.js
+++ 05-lesson/src/components/Header.js
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCount, increaseCount } from '../features/posts/postsSlice'
 
 export const Header = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(getCount)
+
     return (
         <header className="Header">
             <h1>Redux blog</h1>
@@ -12,7 +17,13 @@ export const Header = () => {
                     <li>
                         <Link to="post">Posts</Link>
                     </li>
+                    <li>
+                        <Link to="user">Users</Link>
+                    </li>
                 </ul>
+                <button onClick={() => dispatch(increaseCount())}>
+                    {count}
+                </button>
             </nav>
         </header>
     )
