526c690aa63ae53c2bc63ca58f58f0b742368966
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-12-14 13:31:45 +0100

    adding a count in state, deleting postAdded

--- 05-lesson/src/features/posts/postsSlice.js
+++ 05-lesson/src/features/posts/postsSlice.js
@@ -8,6 +8,7 @@ const initialState = {
     posts: [],
     status: 'idle', // "idle" | "loading" | "succeeded" | "failed"
     error: null,
+    count: 0,
 }
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
@@ -61,35 +62,15 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded: {
-            reducer(state, action) {
-                state.posts.push(action.payload)
-            },
-            prepare(title, content, userId) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        content,
-                        date: new Date().toISOString(),
-                        userId,
-                        reactions: {
-                            thumbsUp: 0,
-                            wow: 0,
-                            heart: 0,
-                            rocket: 0,
-                            coffee: 0,
-                        },
-                    },
-                }
-            },
-        },
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
             const existingPost = state.posts.find((post) => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
+        },
+        increaseCount(state, action) {
+            state.count = state.count + 1
         },
     },
     extraReducers(builder) {
@@ -166,8 +147,10 @@ export const selectPostById = (state, postId) =>
 export const getPostStatus = (state) => state.posts.status
 export const getPostsError = (state) => state.posts.error
 
+export const getCount = (state) => state.posts.count
+
 // actions
-export const { postAdded, reactionAdded } = postsSlice.actions
+export const { increaseCount, reactionAdded } = postsSlice.actions
 
 // reducers
 export default postsSlice.reducer
